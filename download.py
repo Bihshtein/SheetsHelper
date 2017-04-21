bec21dbb941e0d668f58293e76ed8ccc6aa4b883
Alex Bihshtein <alexbihsh@gmail.com>
2017-04-21 21:09:45 +0300

    push sheets helper files

---
+++ download.py
@@ -0,0 +1,59 @@
+import sys
+from oauth2client import client
+import httplib2
+import os
+import io
+from oauth2client.file import Storage
+import json
+from httplib2 import Http
+from apiclient.discovery import build
+from oauth2client import client
+from oauth2client import tools
+import apiclient
+
+
+def GetService():
+    appName = 'sheetassist'
+    file = 'keys.json'
+    scopes = ['https://www.googleapis.com/auth/drive']
+    
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'drive-python-quickstart.json')
+    store = Storage(credential_path)
+    credentials = store.get()
+    
+    if not credentials or credentials.invalid:			
+        flow = client.flow_from_clientsecrets(file, scopes)
+        flow.user_agent = appName    
+        credentials = tools.run_flow(flow,store)
+    http = credentials.authorize (httplib2.Http())
+    return build('drive', 'v3', http=http)
+	
+def DownloadFile(file):
+    drive_service = GetService()  
+    request = drive_service.files().export_media(fileId=file, mimeType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response = request.execute()
+    with open('calendar.xlsx', "wb") as wer:
+        wer.write(response)  
+		
+
+
+days = 2
+if (len(sys.argv) > 1):
+    days = int(sys.argv[1])
+
+editors = {}
+editors['siukei@theculturetrip.com'] = '1AqMxekEh_JerFfeEkY_omaJqsGHgjA5246QsZlfNJBY'
+editors['lily.niu@culturetrip.com'] = '1STKWMSN2yi_Bk-LdS6MimFYMwXQ_z8fRwjPG-zxQFeE'
+editors['tahiera@theculturetrip.com'] = '1_ZEl2HqnKprC-hOIUD6ti79MbBkrdfHxxvNLhWzbBF8'
+editors['grace@culturetrip.com'] = '10RNpzBXpFUcjIABr5eIUO8yxSoP7fBoJ-6oggBeeWug'
+#editors['mariam@theculturetrip.com'] = '1COEqPSZ78R7gOvJbWk1jhMyKz7pI5hmn8cyvRZW4dgs'
+#editors['charlotte.peet@theculturetrip.com'] = '1pFC7mIMhFvN6_6MTWY9tbx-XV6s9Zy8gjpYwlFKXx4c'
+import report
+for editor in editors.iteritems():
+    DownloadFile(editor[1])
+    report.CreateReport(days, editor[0])
+
---
+++ report.py
@@ -0,0 +1,107 @@
+﻿# -*- coding: utf-8 -*-
+import smtplib
+import datetime
+import urllib
+from openpyxl import load_workbook
+from openpyxl import utils
+from oauth2client import client
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def GetMissingIDS(sheet):
+    ids = []
+    for line in sheet.rows:
+        if (line[0].value == 'Submitted' and line[5].value == None):
+            try:
+                ids.append(str(line[1].value))
+            except:
+                print 'failed to parse ...'  
+    return ids
+                        
+def GetMissingUrls(sheet):
+    urls = []
+    for line in sheet.rows:
+        if (line[0].value == 'Published' and line[6].value == None):
+            urls.append(str(line[1].value))
+    return urls
+
+def GetAllSubmitted(sheet):
+    count = 0
+    for line in sheet.rows:
+        if (line[0].value == 'Submitted'):
+            count += 1
+    return count
+
+def GetLastWeek(sheet,days):
+    last = {}
+    for line in sheet.rows:
+        if (line[0].value == 'Published' and line[8].value != None):
+            now = datetime.datetime.now()
+          
+            try:
+                published =  utils.datetime.from_excel(line[8].value)
+            except:
+                try:
+                      published = datetime.datetime.strptime(str(line[8].value),'%Y-%m-%d %H:%M:%S' )
+                except:
+                    print line[8].value
+                    
+                                
+            diff  =  now - published
+            if (diff.days <= days): 
+                try:
+                    last[str(line[1].value)]=str(line[6].value)           
+                except:
+                    print 'failed to parse...'
+    return last
+
+def SendEmail(sheets,urls,ids,last,submitted, days,email):  
+    msg = '<p><b>Active sheets : </b></p>' 
+    for s in sheets:
+        if (sheets.index(s) % 10 == 0):
+            msg += '<div></div>'
+        msg += str(s) + ', '        
+    msg += '<p><b>' + str(submitted) + ' Submitted articles are pending to be published. </b></p>'
+    msg += '<p><b>{1} Published articles in the last {0} days : </b></p>'.format(days,len(last))   
+    for h in last.iteritems():
+        msg += '<div><a href="'+ h[1]+'">'+ h[0] + '</a></div>'
+    msg += '<p><b>' + str(len(ids)) + ' Submitted articles with missing Post ID : </b></p>'
+    for h in ids:
+        msg += '<div>\t' + h + '</div>'                
+    msg +=  '<p><b>'+ str(len(urls))+' Published articles with missing Article URL : </b></p>'
+    for h in urls:
+        msg += '<div>\t' + h + '</div>'        
+    msg = "<html><head></head><body>" + msg + "</html></body>"
+    
+    with open('C:\SheetsHelper\msg.html', "wb") as wer:
+        wer.write(msg)
+            
+    fromAdd = 'bihshtein@hotmail.com'
+    toAdd = ['bihshtein@hotmail.com']
+    emsg = MIMEMultipart('alternative')
+    emsg['Subject'] = "Sheet Report"
+    part2 = MIMEText(msg, 'html')
+    emsg.attach(part2         )
+    s = smtplib.SMTP('smtp.live.com:587')#
+    s.starttls()
+    s.login(fromAdd, 'AlegAleg')
+    s.sendmail(fromAdd, toAdd, emsg.as_string())
+    s.quit()
+                
+def CreateReport(days,email):
+    ids = []
+    urls = []
+    last = {}
+    submitted = 0   
+    wb = load_workbook('C:\SheetsHelper\calendar.xlsx')
+    allSheets = wb.get_sheet_names()
+    unusedSheets = ['Copy Editors & Writers']
+    for sheet in unusedSheets:
+        allSheets.remove(sheet)
+    for sheet in allSheets:   
+        ids += GetMissingIDS(wb[sheet])
+        urls += GetMissingUrls(wb[sheet])
+        for item in GetLastWeek(wb[sheet],days).iteritems():
+            last[item[0]] =item[1] 
+        submitted += GetAllSubmitted(wb[sheet])
+    SendEmail(allSheets,urls,ids, last, submitted,days,email)
